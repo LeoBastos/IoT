4ddb9e2a68116a79c4cf7348e0190cfbefc982c5
Leo Bastos <57710891+LeoBastos@users.noreply.github.com>
2023-02-03 16:36:28 -0300

    Delete teste.py

--- src/lixos/teste.py
+++
@@ -1,8 +0,0 @@
-from urllib.parse import urljoin
-import requests
-from src.config.conf import base_url
-
-data = requests.get(urljoin(base_url, '1'))
-res = data.json()
-print(data.json())
-print(res['Modulos'][0]['name'], res['Modulos'][0]['version'],res['Modulos'][0]['url'], res['Modulos'][0]['isActive'])
